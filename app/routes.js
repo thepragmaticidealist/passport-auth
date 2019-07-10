ea77f604a803d0f5ebdb128cfb8936259072b95e
Mabishi Wakio <elizabeth.wakio@andela.com>
2019-07-10 17:31:52 +0300

    Set up server, routes, index.ejs

---
+++ app/routes.js
@@ -0,0 +1,61 @@
+// Route managing niddleware function
+
+module.exports = (app, passport) => {
+  // Home
+  app.get('/', (req, res, next) => {
+    res.render('index.ejs')
+  });
+
+  // Handle get requests to /login
+  app.get('/login', (req, res, next) => {
+    // all requests will have a req.flash()
+    // pass a local variable i.e. the login flash message to the view
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  /*
+  // Handle post requests to /login
+  // Use passport local strategy as middleware
+  app.post('/login', // passport auth middleware
+  );
+  */
+
+  app.get('/signup', (req, res, next) => {
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage')
+    });
+  });
+
+  /*
+  app.post('/signup',
+  // Passport sign up flow
+  );
+  */
+
+  // Only allow logged in users to visit/get this page
+  // We use the middleware function authenticationCheck to evaluate this
+  app.get('/profile', authenticationCheck, (req, res, next) => {
+    res.render('profile.ejs', {
+      user: req.user
+    });
+  });
+
+  app.get('/logout', (req, res, next) => {
+    req.logout(); // Provided by passport
+    res.redirect('/login');
+  });
+}
+/**
+ * Middleware to check whether a user is authenticated
+ * If authenticated, the method passes control to the next handler
+ * If not, the user is redirected to '/'
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+const authenticationCheck = (req, res, next) => {
+  // isAuthenticated = passport function to check whether a user is authenticated or not
+  req.isAuthenticated() ? next() : res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mongoose = require('mongoose'); // connect to db
+const passport = require('passport'); // auth middleware
+const flash = require('connect-flash'); // middleware for flash messages stored in the session, to store messages to alert a user after a redirect
+// With the flash middleware in place, all requests will have a req.flash() function that can be used for flash messages.
+const logger = require('morgan'); // logging
+const cookieParser = require('cookie-parser'); // session cookie setup to store flash messages. Parses cookie header and populates req.cookies with cookies
+const bodyParser = require('body-parser'); // middleware that adds body object to request object
+const session = require('express-session'); // session setup to store flash messages
+
+const app = express();
+const router = require('./app/routes.js')
+const environment = process.env.NODE_ENV;
+const PORT = process.env.PORT || 8000;
+const configDB = require('./config/database');
+
+mongoose.connect(configDB.url, {
+      useNewUrlParser: true
+    }, (err) => {
+  if (err) {
+    console.log(`Error connecting to ${configDB.url}`);
+  } else {
+    console.log(`Successfully connected to mongoDB`);
+  }
+});
+
+// Pass passport into config function for it to be configured
+// const passportConfig = require('./config/passport');
+// passportConfig(passport);
+
+// Request logging
+if (environment !== 'production') {
+  app.use(logger('dev'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended : true
+}));
+
+/*
+// For flash messages, optional if implentation not for browser
+app.use(session({
+  secret: 'Quis custodiet ipsos custodes'
+}))
+app.use(cookieParser());
+app.use(flash()); // Special area of the session used for storing messages, i.e. flash messages stores in session
+
+app.set('view engine', 'ejs');
+
+// For passport
+app.use(passport.initialize());
+app.use(passport.session());
+*/
+
+// Initialise routes
+router(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.listen(PORT, () => {
+  console.log(`Server now listening at http://localhost:${PORT}`);
+});
