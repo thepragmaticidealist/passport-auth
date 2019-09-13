a49433e9da023bbc2bf4b403b70ae6f9d7a8b0f3
Mabishi Wakio <elizabeth.wakio@andela.com>
2019-09-13 15:55:53 +0300

    Add implicit return arrow fx

--- config/unlink.js
+++ config/unlink.js
@@ -3,24 +3,24 @@ module.exports = {
     const user = req.user;
     user.local.email = null;
     user.local.password = null;
-    user.save(err => res.redirect('/profile'))
+    user.save(_err => res.redirect('/profile'))
   },
 
   facebook: (req, res, _next) => {
     const user = req.user;
     user.facebook.token = null;
-    user.save(err =>  res.redirect('/profile'))
+    user.save(_err => res.redirect('/profile'))
   },
 
   google: (req, res, _next) => {
     const user = req.user;
     user.google.token = null;
-    user.save(err => res.redirect('/profile'))
+    user.save(_err => res.redirect('/profile'))
   },
 
   github: (req, res, _next) => {
     const user = req.user;
     user.github.token = null;
-    user.save(err => res.redirect('/profile'))
+    user.save(_err => res.redirect('/profile'))
   }
 }
